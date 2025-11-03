ccae45f3013634284a1eb4a77dcecf6c233fcad5
Haku021 <jgonzalera@unsa.edu.pe>
2025-11-03 16:53:07 -0500

    Ejercicio 12

--- ejercicio12.js
+++ ejercicio12.js
@@ -3,12 +3,16 @@ class Producto{
     #precio;
     #stock;
     set precio(precio){
+        precio=Number(precio);
         if(precio<=0){
             console.log("Precio invalido");
             return;
         }
         else
             this.#precio=precio;
+    }
+    get precio(){
+        return "$"+ this.#precio.toFixed(2);
     }
     set stock(stock){
         if(stock<=0){
