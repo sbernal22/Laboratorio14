85fb16bf0465e530d549aebfbce02531cd102adc
Haku021 <jgonzalera@unsa.edu.pe>
2025-11-03 16:12:44 -0500

    Ejercicio 6

---
+++ ejercicio6.js
@@ -0,0 +1,4 @@
+let numero=parseFloat(prompt("Ingrese un numero"));
+console.log("Cuadrado: "+(Math.pow(numero, 2)));
+console.log("Cubo: "+(Math.pow(numero, 3)));
+console.log("Raiz: "+(Math.sqrt(numero, 2)));
